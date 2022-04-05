ede3cb1157ce206758ffcb2d0d8b2855c10854cb
Tatsuya Koishi <tkoishi@b-shock.co.jp>
2022-04-05 18:08:13 +0900

    .

--- app/lib/kariyon/logger.rb
+++ app/lib/kariyon/logger.rb
@@ -1,5 +1,15 @@
 module Kariyon
   class Logger < Ginseng::Logger
     include Package
+
+    def info(message)
+      puts message
+      super
+    end
+
+    def error(message)
+      warn message
+      super
+    end
   end
 end
